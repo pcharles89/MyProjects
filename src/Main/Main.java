6871a249d4b1a69e84c24640649fc497e9f6452e
pcharles89 <93538393+pcharles89@users.noreply.github.com>
2021-11-16 07:28:34 -0500

    Initial commit

---
+++ src/Main/Main.java
@@ -0,0 +1,25 @@
+package Main;
+
+import DAO.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/FirstScreen.fxml"));
+        stage.setTitle("My First Screen");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        JDBC.openConnection();
+        launch(args);
+    }
+}
