7c3de902a255daa0f2d689cc66adc2f185949e0b
pcharles89 <93538393+pcharles89@users.noreply.github.com>
2021-11-16 17:14:33 -0500

    Second Commit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         JDBC.openConnection();
+        System.out.println("Hi");
         launch(args);
     }
 }
